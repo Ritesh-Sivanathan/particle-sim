14b44551bf636eaf17d12c47a3ba18b74ffdca59
Ritesh <ritesh.sivanathan@gmail.com>
2025-05-22 14:11:41 -0400

    better particle-bound logic

--- basic/experiments/exp_collision_impr.py
+++ basic/experiments/exp_collision_impr.py
@@ -12,11 +12,25 @@ for i in range(500):
         p1.change_velocity(1,1,1)
 
     if p1.position.real >= 10:
-        p1.change_velocity(-1, -0.5, 1)
-        print(f"collision x:{p1.position.real} y:{p1.position.imag}")
-    elif p1.position.real <= 0 and i!=1:
-        p1.change_velocity(1, 0.5, 1)
-        print(f"collision x:{p1.position.real} y:{p1.position.imag}")
+        random_y = np.random.random()
+        p1.change_velocity(-1, -random_y, 1)
+        # print(f"collision x:{p1.position.real} y:{p1.position.imag}")
+
+    if p1.position.imag >= 10:
+        random_y = np.random.random()
+        p1.change_velocity(1, -random_y, 1)
+
+    if p1.position.imag <= 0:
+        random_y = np.random.random()
+        p1.change_velocity(1, random_y, 1)
+
+    elif p1.position.real <= 0 and i!=0:
+        random_y = np.random.random()
+        p1.change_velocity(1, random_y, 1)
+        # print(f"collision x:{p1.position.real} y:{p1.position.imag}")
+
+    if p1.position.real > 10:
+        print(p1.position.real)
 
     p1.update_particle_position()
 
@@ -24,5 +38,11 @@ for i in range(500):
     data[1].append(p1.position.imag)
 
 fig, ax = plt.subplots()
-plt.plot(data[1], data[0])
+
+plt.axvline(x=0)
+plt.axvline(x=10)
+plt.axhline(y=0)
+plt.axhline(y=10)
+
+plt.plot(data[0], data[1])
 plt.show()

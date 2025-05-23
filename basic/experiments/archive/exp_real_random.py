4851d2d9b629b74eeaa87d9584728211570fa19d
Ritesh <ritesh.sivanathan@gmail.com>
2025-05-23 00:14:03 -0400

    moved old experiments to archive

---
+++ basic/experiments/archive/exp_real_random.py
@@ -0,0 +1,32 @@
+'''
+
+More realistic random movement.
+
+'''
+
+from ...particle import Particle
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+p = Particle()
+data = [[], []]
+
+x,y = 0,0
+vx, vy = 0,0
+
+for _ in range(500):
+    
+    vx = np.random.normal(0, 0.5)
+    vy = np.random.normal(0, 0.5)
+    
+    p.change_velocity(vx, vy, np.random.uniform(0.01, 0.03))
+
+    px, py = p.update_particle_position()
+
+    data[0].append(px)
+    data[1].append(py)
+
+fig, ax = plt.subplots()
+ax.plot(data[1], data[0])
+plt.show()

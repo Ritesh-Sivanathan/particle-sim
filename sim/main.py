410f071914b50132421338f50b4dd187a6daea88
Ritesh <ritesh.sivanathan@gmail.com>
2025-05-31 01:11:27 -0400

    testing out system

--- sim/main.py
+++ sim/main.py
@@ -1,4 +1,5 @@
 from particle import Particle
+from system import System
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,10 +7,14 @@ vix = np.random.randint(1,25)
 viy = np.random.randint(1,25)
 
 particle = Particle(vx=vix, vy=viy, max_x=10500, max_y=10500)
+system = System(particles=[particle],)
 
-data = particle.loop(50000)
+data = system.simulate()
+print(data[0][0])
 
-fig, ax = plt.subplots()
-plt.plot(data[0], data[1])
-plt.title(f"vix={vix} viy={viy}")
-plt.show()
+# data = particle.loop(50000)
+
+# fig, ax = plt.subplots()
+# plt.plot(data[0], data[1])
+# plt.title(f"vix={vix} viy={viy}")
+# plt.show()
